2c30b6d638b96df5b4434c3ed82900b9e4f7c638
Sergio081096 <31215551+Sergio081096@users.noreply.github.com>
2017-08-25 20:57:42 -0500

    termina

---
+++ Practica2-1/main.cpp
@@ -0,0 +1,106 @@
+//Semestre 2018-1
+//************************************************************//
+//************************************************************//
+//************** Alumno (s): *********************************//
+//*************	Cano Olguin Luis Sergio ******//
+//*************	Version de trabajo Visual 2012 ******//
+//************************************************************//
+#include "Main.h"
+void InitGL ( GLvoid )     // Inicializamos parametros
+{
+	//glShadeModel(GL_FLAT);							// Habilitamos Smooth Shading
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);				// Negro de fondo
+	//glClearDepth(1.0f);									// Configuramos Depth Buffer
+	//glEnable(GL_DEPTH_TEST);							// Habilitamos Depth Testing
+	//glDepthFunc(GL_LEQUAL);								// Tipo de Depth Testing a realizar
+	
+	
+}
+
+void display ( void )   // Creamos la funcion donde se dibuja
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	// Limiamos pantalla y Depth Buffer	
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();									// Reinicializamos la actual matriz Modelview
+
+	//Poner aqui codigo ha dibujar
+	//glPointSize(10.0);//tamaño del punto
+	glBegin(GL_POLYGON);//pluar,puntos(GL_POINTS), lineas(GL_LINES), todos unicdos(GL_LINE_LOOP),triangulos(GL_TRIANGLES),poligono(GL_POLYGON)
+	glColor3f(1.0,0.0,0.0);//desde aqui empieza el color y continua hasta que encuentre otro color
+	glVertex3f(130.0,- 20.0, -1.2);//1  3 ejes de coordenadas ---flotantes
+	glVertex3f(150.0, -90.0, -1.2);//2	
+	glVertex3f(110.0, -90.0, -1.2);//10
+	glEnd();//termina
+	glColor3f(1.0,1.0,0.0);
+	glBegin(GL_POLYGON);
+	glVertex3f(150.0, -90.0, -1.2);//2	
+	glVertex3f(210.0, -90.0, -1.2);//3
+	glVertex3f(160.0, -130.0, -1.2);//4
+	glEnd();
+	glColor3f(0.0,0.0,1.0);
+	glBegin(GL_POLYGON);
+	glVertex3f(160.0, -130.0, -1.2);//4
+	glVertex3f(180.0, -200.0, -1.2);//5/*	
+	glVertex3f(130.0, -160.0, -1.2);//6
+	glEnd();
+	glColor3f(0.0,1.0,0.0);
+	glBegin(GL_POLYGON);
+	glVertex3f(130.0, -160.0, -1.2);//6
+	glVertex3f(80.0, -200.0, -1.2);//7
+	glVertex3f(100.0, -130.0, -1.2);//8
+	glEnd();
+	glColor3f(1.0,0.0,1.0);
+	glBegin(GL_POLYGON);
+	glVertex3f(100.0, -130.0, -1.2);//8
+	glVertex3f(50.0, -90.0, -1.2);//9
+	glVertex3f(110.0, -90.0, -1.2);//10
+	glEnd();//termina*/
+	glFlush();
+}
+
+void reshape ( int width , int height )   // Creamos funcion Reshape
+{
+	if (height==0)										// Prevenir division entre cero
+	{
+		height=1;
+	}
+
+	glViewport(0,0,width,height);
+
+	glMatrixMode(GL_PROJECTION);						// Seleccionamos Projection Matrix
+	glLoadIdentity();
+
+	// Ortogonal
+	glOrtho(0,250,-250,0,0.1,2);//dimenciones
+	//horizontal, vertical,---
+
+	glMatrixMode(GL_MODELVIEW);							// Seleccionamos Modelview Matrix
+	glLoadIdentity();									
+}
+
+// Termina la ejecucion del programa cuando se presiona ESC
+void keyboard(unsigned char key, int x, int y)
+{
+	 switch (key) 
+	 {
+		case 27: exit(0);
+				 break;
+	 }
+	  glutPostRedisplay();
+}    
+
+int main ( int argc, char** argv )   // Main Function
+{
+  glutInit            (&argc, argv); // Inicializamos OpenGL
+  glutInitDisplayMode (GLUT_RGBA | GLUT_SINGLE); // Display Mode (Clores RGB y alpha | Buffer Sencillo )
+  glutInitWindowSize  (500, 500);	// Tamaño de la Ventana
+  glutInitWindowPosition (0, 0);	//Posicion de la Ventana
+  glutCreateWindow    ("Practica 2"); // Nombre de la Ventana
+  InitGL ();						// Parametros iniciales de la aplicacion
+  glutDisplayFunc     ( display );  //Indicamos a Glut función de dibujo
+  glutReshapeFunc     ( reshape );	//Indicamos a Glut función en caso de cambio de tamano
+  glutKeyboardFunc	  (keyboard);	//Indicamos a Glut función de manejo de teclado
+  glutMainLoop        ( );          // 
+
+  return 0;
+}
